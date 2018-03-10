bdce69187c8e6c404311a473220a585238643df7
victor hazbun <victorhazbun87@gmail.com>
2018-03-10 01:29:35 -0500

    Project behaves like sizeable

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 
 RSpec.describe Project do
 
+  it_should_behave_like "sizeable"
+
   describe "completion" do
     let(:project) { Project.new }
     let(:task) { Task.new }
@@ -41,11 +43,12 @@ RSpec.describe Project do
     end
 
     it "can calculate total size" do
-      expect(project.total_size).to eq(10)
+      expect(project).to be_of_size(10)
+      expect(project).not_to be_of_size(5)
     end
 
     it "can calculate remaining size" do
-      expect(project.remaining_size).to eq(5)
+      expect(project).to be_of_size(5).for_incomplete_tasks_only
     end
 
     it "knows its velocity" do
