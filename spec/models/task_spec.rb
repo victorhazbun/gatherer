b72907c0daa8a67512f32e043981d0815c663cba
victor hazbun <victorhazbun87@gmail.com>
2018-03-16 20:36:53 -0500

    Create and reorder task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -15,6 +15,14 @@ RSpec.describe Task do
       task.mark_completed
       expect(task).to be_complete
     end
+
+    it "stubs with multiple return values" do
+      task = Task.new
+      allow(task).to receive(:size).and_return(1, 2)
+      assert_equal(1, task.size)
+      assert_equal(2, task.size)
+      assert_equal(2, task.size)
+    end
   end
 
   describe "velocity" do
@@ -37,4 +45,50 @@ RSpec.describe Task do
       expect(task.points_toward_velocity).to eq(0)
     end
   end
+
+  describe "order", aggregate_failures: true do
+    let(:project) { create(:project, name: "Project") }
+    let!(:first) { project.tasks.create!(project_order: 1) }
+    let!(:second) { project.tasks.create!(project_order: 2) }
+    let!(:third) { project.tasks.create!(project_order: 3) }
+
+    it "can determine that a task is first or last" do
+      expect(first).to be_first_in_project
+      expect(first).not_to be_last_in_project
+      expect(second).not_to be_first_in_project
+      expect(second).not_to be_last_in_project
+      expect(third).not_to be_first_in_project
+      expect(third).to be_last_in_project
+    end
+
+    it "knows neighbors" do
+      expect(second.previous_task).to eq(first)
+      expect(second.next_task).to eq(third)
+      expect(first.previous_task).to be_nil
+      expect(third.next_task).to be_nil
+    end
+
+    it "can move up" do
+      second.move_up
+      expect(first.reload.project_order).to eq(2)
+      expect(second.reload.project_order).to eq(1)
+    end
+
+    it "can move down" do
+      second.move_down
+      expect(third.reload.project_order).to eq(2)
+      expect(second.reload.project_order).to eq(3)
+    end
+
+    it "handles edge case moves up" do
+      first.move_up
+      expect(first.reload.project_order).to eq(1)
+    end
+
+    it "handles edge case moves down" do
+      third.move_down
+      expect(third.reload.project_order).to eq(3)
+    end
+  end
+
 end
---
+++ spec/system/add_task_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "adding a new task" do
+  let!(:project) { create(:project, name: "Project Bluebook") }
+  let!(:task_1) { create(
+    :task, project: project, title: "Search Sky", size: 1, project_order: 1) }
+  let!(:task_2) { create(
+    :task, project: project, title: "Use Telescope",
+           size: 1, project_order: 2) }
+
+  it "can add and reorder a task" do
+    visit(project_path(project))
+    fill_in("Task", with: "Find UFOs")
+    select("2", from: "Size")
+    click_on("Add Task")
+    expect(current_path).to eq(project_path(project))
+    within("#task_3") do
+      expect(page).to have_selector(".name", text: "Find UFOs")
+      expect(page).to have_selector(".size", text: "2")
+      expect(page).not_to have_selector("a", text: "Down")
+      click_on("Up")
+    end
+    expect(current_path).to eq(project_path(project))
+    within("#task_2") do
+      expect(page).to have_selector(".name", text: "Find UFOs")
+    end
+  end
+
+end
