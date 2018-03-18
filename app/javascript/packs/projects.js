a1d77d4da4611d06dfe41405245625b4b848ff0e
victor hazbun <victorhazbun87@gmail.com>
2018-03-17 21:19:56 -0500

    Build Project & tasks front-end with ES6 & JQuery
    RSpec tests needs refactor since this changes broke them

--- app/javascript/packs/projects.js
+++ app/javascript/packs/projects.js
@@ -1,62 +1,13 @@
-export class Project {
-  constructor() {
-    this.tasks = []
-  }
+import {Project} from '../../../app/javascript/packs/project.js'
+import {ProjectTable} from '../../../app/javascript/packs/project_table.js'
 
-  appendTask(task) {
-    this.tasks.push(task)
-    task.project = this
-    task.index = this.tasks.length - 1
+$(() => {
+  if ($('.task-table').length > 0) {
+    const element = $('.task-table')
+    const project = new Project(element.data('project'))
+    project.load().then(() => {
+      const table = new ProjectTable(project, '.task-table')
+      table.insert()
+    })
   }
-
-  firstTask() {
-    return this.tasks[0]
-  }
-
-  lastTask() {
-    return this.tasks[this.tasks.length - 1]
-  }
-
-  swapTasksAt(index1, index2) {
-    const temp = this.tasks[index1]
-    this.tasks[index1] = this.tasks[index2]
-    this.tasks[index2] = temp
-  }
-}
-
-export class Task {
-  constructor(name, size) {
-    this.name = name
-    this.size = size
-    this.project = null
-    this.index = null
-  }
-
-  isFirst() {
-    if (this.project) {
-      return this.project.firstTask() === this
-    }
-    return false
-  }
-
-  isLast() {
-    if (this.project) {
-      return this.project.lastTask() === this
-    }
-    return false
-  }
-
-  moveUp() {
-    if (this.isFirst()) {
-      return
-    }
-    this.project.swapTasksAt(this.index - 1, this.index)
-  }
-
-  moveDown() {
-    if (this.isLast()) {
-      return
-    }
-    this.project.swapTasksAt(this.index, this.index + 1)
-  }
-}
+})
