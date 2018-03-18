88561532533f0d516718c7404182e7f92593e320
victor hazbun <victorhazbun87@gmail.com>
2018-03-18 01:47:21 -0500

    Implement project name with status helper

---
+++ spec/views/projects/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe "projects/index" do
+
+  let(:completed_task) { Task.create!(completed_at: 1.day.ago, size: 1) }
+  let(:on_schedule) { Project.create!(
+    due_date: 1.year.from_now, name: "On Schedule", tasks: [completed_task]) }
+  let(:incomplete_task) { Task.create!(size: 1) }
+  let(:behind_schedule) { Project.create!(
+    due_date: 1.day.from_now, name: "Behind Schedule",
+    tasks: [incomplete_task]) }
+
+  it "renders the index page with correct dom elements" do
+    @projects = [on_schedule, behind_schedule]
+    render
+    expect(rendered).to have_selector(
+      "#project_#{on_schedule.id} .on_schedule")
+    expect(rendered).to have_selector(
+      "#project_#{behind_schedule.id} .behind_schedule")
+  end
+
+end
